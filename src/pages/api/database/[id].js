e3f6bf2bbe64282fdc73b5c6fc822f0d46fa69f4
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-06-29 21:05:58 -0300

    feat: :sparkles: adicionando GETs iniciais dos databases

--- src/pages/api/database/[id].js
+++ src/pages/api/database/[id].js
@@ -1,10 +1,30 @@
-export default function handler(req, res) {
-    const {
-        query: { id },
-    } = req
+const { Client } = require('@notionhq/client')
 
-    res.status(200).json({
-        id,
-        info: 'This endpoint is in development'
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN
+})
+
+async function getTasks(id) {
+    const response = await notion.databases.query({
+        database_id: id,
     })
+
+    return response.results
+}
+
+export default async function handler(req, res) {
+    const { method } = req
+    const { id } = req.query
+
+    switch (method) {
+        case 'GET':
+            try {
+                const tasks = await getTasks(id)
+                res.status(200).json(tasks)
+            }
+            catch (error) {
+                res.status(500).json({ statusCode: 500, message: error.message })
+            }
+            break
+    }
 }
